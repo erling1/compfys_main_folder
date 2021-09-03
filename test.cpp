2e96e5f40060bfec791f3836f66b0b33e27b2a49
erling1 <nupen.erling@gmail.com>
2021-09-03 08:16:51 +0200

    test

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include iostream;
+
+using namespace std;
+
+int main(){
+  string characterName = "John";
+  int characterAge;
+  characterAge = 35;
+
+  cout << "There once was a man named " <<characterName << endl;
+  cout << "He was 35 years old" << endl;
+  cout << "He liked the name John" << endl;
+  cout << "But did not like being 35" << endl;
+
+
+}
